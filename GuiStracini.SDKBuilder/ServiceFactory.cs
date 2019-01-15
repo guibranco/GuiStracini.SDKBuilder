883bed698ea374ebb2cd58d7e753493096f30e10
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2019-01-15 09:54:22 -0200

    Update pacakges

--- GuiStracini.SDKBuilder/ServiceFactory.cs
+++ GuiStracini.SDKBuilder/ServiceFactory.cs
@@ -2,5 +2,24 @@
 {
     public class ServiceFactory : IServiceFactory
     {
+        #region Private fields
+
+
+        #endregion
+
+        #region ~Ctors
+
+
+        #endregion
+
+        #region Private methods
+
+
+        #endregion
+
+        #region Pulic methods
+
+
+        #endregion
     }
 }
