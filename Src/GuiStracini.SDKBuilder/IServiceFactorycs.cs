58e7fbf90e71d75376c186364256f9a003ab639a
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2023-01-14 21:01:50 +0400

    Fix namespace

--- Src/GuiStracini.SDKBuilder/IServiceFactorycs.cs
+++ Src/GuiStracini.SDKBuilder/IServiceFactorycs.cs
@@ -1,120 +1,119 @@
-﻿namespace GuiStracini.SDKBuilder
+﻿namespace GuiStracini.SDKBuilder;
+
+using System.Threading;
+using System.Threading.Tasks;
+
+/// <summary>
+/// The service factory interface.
+/// </summary>
+public interface IServiceFactory
 {
-    using System.Threading;
-    using System.Threading.Tasks;
+    /// <summary>
+    /// Heads the specified data.
+    /// </summary>
+    /// <typeparam name="TIn">The type of the in.</typeparam>
+    /// <param name="data">The data.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns></returns>
+    ValueTask<TIn> Head<TIn>(TIn data, CancellationToken cancellationToken)
+        where TIn : BaseRequest;
 
     /// <summary>
-    /// The service factory interface.
+    /// Gets the specified data.
     /// </summary>
-    public interface IServiceFactory
-    {
-        /// <summary>
-        /// Heads the specified data.
-        /// </summary>
-        /// <typeparam name="TIn">The type of the in.</typeparam>
-        /// <param name="data">The data.</param>
-        /// <param name="cancellationToken">The cancellation token.</param>
-        /// <returns></returns>
-        ValueTask<TIn> Head<TIn>(TIn data, CancellationToken cancellationToken)
-            where TIn : BaseRequest;
+    /// <typeparam name="TIn">The type of the in.</typeparam>
+    /// <param name="data">The data.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns></returns>
+    ValueTask<TIn> Get<TIn>(TIn data, CancellationToken cancellationToken)
+        where TIn : BaseRequest;
 
-        /// <summary>
-        /// Gets the specified data.
-        /// </summary>
-        /// <typeparam name="TIn">The type of the in.</typeparam>
-        /// <param name="data">The data.</param>
-        /// <param name="cancellationToken">The cancellation token.</param>
-        /// <returns></returns>
-        ValueTask<TIn> Get<TIn>(TIn data, CancellationToken cancellationToken)
-            where TIn : BaseRequest;
+    /// <summary>
+    /// Gets the specified data.
+    /// </summary>
+    /// <typeparam name="TIn">The type of the in.</typeparam>
+    /// <typeparam name="TOut">The type of the out.</typeparam>
+    /// <param name="data">The data.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns></returns>
+    ValueTask<TOut> Get<TIn, TOut>(TIn data, CancellationToken cancellationToken)
+        where TIn : BaseRequest
+        where TOut : BaseResponse;
 
-        /// <summary>
-        /// Gets the specified data.
-        /// </summary>
-        /// <typeparam name="TIn">The type of the in.</typeparam>
-        /// <typeparam name="TOut">The type of the out.</typeparam>
-        /// <param name="data">The data.</param>
-        /// <param name="cancellationToken">The cancellation token.</param>
-        /// <returns></returns>
-        ValueTask<TOut> Get<TIn, TOut>(TIn data, CancellationToken cancellationToken)
-            where TIn : BaseRequest
-            where TOut : BaseResponse;
+    /// <summary>
+    /// Posts the specified data.
+    /// </summary>
+    /// <typeparam name="TIn">The type of the in.</typeparam>
+    /// <param name="data">The data.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns></returns>
+    ValueTask<TIn> Post<TIn>(TIn data, CancellationToken cancellationToken)
+        where TIn : BaseRequest;
 
-        /// <summary>
-        /// Posts the specified data.
-        /// </summary>
-        /// <typeparam name="TIn">The type of the in.</typeparam>
-        /// <param name="data">The data.</param>
-        /// <param name="cancellationToken">The cancellation token.</param>
-        /// <returns></returns>
-        ValueTask<TIn> Post<TIn>(TIn data, CancellationToken cancellationToken)
-            where TIn : BaseRequest;
+    /// <summary>
+    /// Posts the specified data.
+    /// </summary>
+    /// <typeparam name="TIn">The type of the in.</typeparam>
+    /// <typeparam name="TOut">The type of the out.</typeparam>
+    /// <param name="data">The data.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns></returns>
+    ValueTask<TOut> Post<TIn, TOut>(TIn data, CancellationToken cancellationToken)
+        where TIn : BaseRequest
+        where TOut : BaseResponse;
 
-        /// <summary>
-        /// Posts the specified data.
-        /// </summary>
-        /// <typeparam name="TIn">The type of the in.</typeparam>
-        /// <typeparam name="TOut">The type of the out.</typeparam>
-        /// <param name="data">The data.</param>
-        /// <param name="cancellationToken">The cancellation token.</param>
-        /// <returns></returns>
-        ValueTask<TOut> Post<TIn, TOut>(TIn data, CancellationToken cancellationToken)
-            where TIn : BaseRequest
-            where TOut : BaseResponse;
+    /// <summary>
+    /// Puts the specified data.
+    /// </summary>
+    /// <typeparam name="TIn">The type of the in.</typeparam>
+    /// <param name="data">The data.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns></returns>
+    ValueTask<TIn> Put<TIn>(TIn data, CancellationToken cancellationToken)
+        where TIn : BaseRequest;
 
-        /// <summary>
-        /// Puts the specified data.
-        /// </summary>
-        /// <typeparam name="TIn">The type of the in.</typeparam>
-        /// <param name="data">The data.</param>
-        /// <param name="cancellationToken">The cancellation token.</param>
-        /// <returns></returns>
-        ValueTask<TIn> Put<TIn>(TIn data, CancellationToken cancellationToken)
-            where TIn : BaseRequest;
+    /// <summary>
+    /// Puts the specified data.
+    /// </summary>
+    /// <typeparam name="TIn">The type of the in.</typeparam>
+    /// <typeparam name="TOut">The type of the out.</typeparam>
+    /// <param name="data">The data.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns></returns>
+    ValueTask<TOut> Put<TIn, TOut>(TIn data, CancellationToken cancellationToken)
+        where TIn : BaseRequest
+        where TOut : BaseResponse;
 
-        /// <summary>
-        /// Puts the specified data.
-        /// </summary>
-        /// <typeparam name="TIn">The type of the in.</typeparam>
-        /// <typeparam name="TOut">The type of the out.</typeparam>
-        /// <param name="data">The data.</param>
-        /// <param name="cancellationToken">The cancellation token.</param>
-        /// <returns></returns>
-        ValueTask<TOut> Put<TIn, TOut>(TIn data, CancellationToken cancellationToken)
-            where TIn : BaseRequest
-            where TOut : BaseResponse;
+    /// <summary>
+    /// Patches the specified data.
+    /// </summary>
+    /// <typeparam name="TIn">The type of the in.</typeparam>
+    /// <param name="data">The data.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns></returns>
+    ValueTask<TIn> Patch<TIn>(TIn data, CancellationToken cancellationToken)
+        where TIn : BaseResponse;
 
-        /// <summary>
-        /// Patches the specified data.
-        /// </summary>
-        /// <typeparam name="TIn">The type of the in.</typeparam>
-        /// <param name="data">The data.</param>
-        /// <param name="cancellationToken">The cancellation token.</param>
-        /// <returns></returns>
-        ValueTask<TIn> Patch<TIn>(TIn data, CancellationToken cancellationToken)
-            where TIn : BaseResponse;
+    /// <summary>
+    /// Patches the specified data.
+    /// </summary>
+    /// <typeparam name="TIn">The type of the in.</typeparam>
+    /// <typeparam name="TOut">The type of the out.</typeparam>
+    /// <param name="data">The data.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns></returns>
+    ValueTask<TOut> Patch<TIn, TOut>(TIn data, CancellationToken cancellationToken)
+        where TIn : BaseRequest
+        where TOut : BaseResponse;
 
-        /// <summary>
-        /// Patches the specified data.
-        /// </summary>
-        /// <typeparam name="TIn">The type of the in.</typeparam>
-        /// <typeparam name="TOut">The type of the out.</typeparam>
-        /// <param name="data">The data.</param>
-        /// <param name="cancellationToken">The cancellation token.</param>
-        /// <returns></returns>
-        ValueTask<TOut> Patch<TIn, TOut>(TIn data, CancellationToken cancellationToken)
-            where TIn : BaseRequest
-            where TOut : BaseResponse;
+    /// <summary>
+    /// Deletes the specified data.
+    /// </summary>
+    /// <typeparam name="TIn">The type of the in.</typeparam>
+    /// <param name="data">The data.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns></returns>
+    ValueTask<TIn> Delete<TIn>(TIn data, CancellationToken cancellationToken)
+        where TIn : BaseRequest;
 
-        /// <summary>
-        /// Deletes the specified data.
-        /// </summary>
-        /// <typeparam name="TIn">The type of the in.</typeparam>
-        /// <param name="data">The data.</param>
-        /// <param name="cancellationToken">The cancellation token.</param>
-        /// <returns></returns>
-        ValueTask<TIn> Delete<TIn>(TIn data, CancellationToken cancellationToken)
-            where TIn : BaseRequest;
-
-    }
 }
--- Src/GuiStracini.SDKBuilder/Routing/EndpointRouteAttribute.cs
+++ Src/GuiStracini.SDKBuilder/Routing/EndpointRouteAttribute.cs
@@ -1,36 +1,35 @@
-﻿namespace GuiStracini.SDKBuilder.Routing
+﻿namespace GuiStracini.SDKBuilder.Routing;
+
+using System;
+
+/// <summary>
+/// Class EndpointRouteAttribute.
+/// </summary>
+/// <seealso cref="System.Attribute" />
+[AttributeUsage(AttributeTargets.Class)]
+public class EndpointRouteAttribute : Attribute
 {
-    using System;
+    #region ~Ctor
 
     /// <summary>
-    /// Class EndpointRouteAttribute.
+    /// Initializes a new instance of the <see cref="EndpointRouteAttribute"/> class.
     /// </summary>
-    /// <seealso cref="System.Attribute" />
-    [AttributeUsage(AttributeTargets.Class)]
-    public class EndpointRouteAttribute : Attribute
+    /// <param name="endPoint">The end point path of the request.</param>
+    public EndpointRouteAttribute(string endPoint)
     {
-        #region ~Ctor
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="EndpointRouteAttribute"/> class.
-        /// </summary>
-        /// <param name="endPoint">The end point path of the request.</param>
-        public EndpointRouteAttribute(string endPoint)
-        {
-            EndPoint = endPoint;
-        }
-
-        #endregion
-
-        #region Public properties
-
-        /// <summary>
-        /// Gets the end point.
-        /// </summary>
-        /// <value>The end point path.</value>
-        public string EndPoint { get; }
-
-        #endregion
-
+        EndPoint = endPoint;
     }
+
+    #endregion
+
+    #region Public properties
+
+    /// <summary>
+    /// Gets the end point.
+    /// </summary>
+    /// <value>The end point path.</value>
+    public string EndPoint { get; }
+
+    #endregion
+
 }
