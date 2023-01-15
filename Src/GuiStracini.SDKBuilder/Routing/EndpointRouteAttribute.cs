c11347d46bd58f22585391d294cd9716c0a241a6
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2023-01-15 19:37:45 +0400

    Block scope namespace

--- Src/GuiStracini.SDKBuilder/Routing/EndpointRouteAttribute.cs
+++ Src/GuiStracini.SDKBuilder/Routing/EndpointRouteAttribute.cs
@@ -1,34 +1,35 @@
-﻿namespace GuiStracini.SDKBuilder.Routing;
-
-using System;
-
-/// <summary>
-/// Class EndpointRouteAttribute.
-/// </summary>
-/// <seealso cref="System.Attribute" />
-[AttributeUsage(AttributeTargets.Class)]
-public class EndpointRouteAttribute : Attribute
+﻿namespace GuiStracini.SDKBuilder.Routing
 {
-    #region ~Ctor
+    using System;
 
     /// <summary>
-    /// Initializes a new instance of the <see cref="EndpointRouteAttribute"/> class.
+    /// Class EndpointRouteAttribute.
     /// </summary>
-    /// <param name="endPoint">The end point path of the request.</param>
-    public EndpointRouteAttribute(string endPoint)
+    /// <seealso cref="System.Attribute" />
+    [AttributeUsage(AttributeTargets.Class)]
+    public class EndpointRouteAttribute : Attribute
     {
-        EndPoint = endPoint;
+        #region ~Ctor
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EndpointRouteAttribute"/> class.
+        /// </summary>
+        /// <param name="endPoint">The end point path of the request.</param>
+        public EndpointRouteAttribute(string endPoint)
+        {
+            EndPoint = endPoint;
+        }
+
+        #endregion
+
+        #region Public properties
+
+        /// <summary>
+        /// Gets the end point.
+        /// </summary>
+        /// <value>The end point path.</value>
+        public string EndPoint { get; }
+
+        #endregion
     }
-
-    #endregion
-
-    #region Public properties
-
-    /// <summary>
-    /// Gets the end point.
-    /// </summary>
-    /// <value>The end point path.</value>
-    public string EndPoint { get; }
-
-    #endregion
 }
